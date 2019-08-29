e9c696f2fee35290eb64a5f09b6840e80b069d43
Schlomo Schapiro <schlomo+git@schapiro.org>
2019-08-29 16:44:55 +0200

    Move git version handling to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,59 @@
-const outDir = "public";
-const gulp = require('gulp');
-const { series, src, watch } = require('gulp');
-const $ = require('gulp-load-plugins')();
-const fb = require('firebase-tools');
+const
+  outDir = "public",
+  { series, src, dest, watch } = require('gulp'),
+  $ = require('gulp-load-plugins')(),
+  fb = require('firebase-tools'),
+  { gitDescribeSync } = require('git-describe')
+  ;
 
-function clean() {
-  return require('del')([outDir]);
+function getGitVersion(fallback="not-git-repo-and-VERSION-not-set") {
+  var version = process.env.VERSION || fallback;
+  var dir = __dirname;
+  try {
+    version = gitDescribeSync(dir).raw;
+  } catch (error) {
+    console.warn(`${dir} is not a git repository, using ${version} as version`)
+  }
+  return version;
 }
 
 
+const version = getGitVersion();
+console.log(`Version: ${version}`);
 const versioncss = `
 
 /* appended by gulp */
 #version::after {
-  content: " (${ process.env.VERSION || "unknown VERSION" })";
+  content: " ${version}";
 }
 
+/*
 #version:hover::after {
   content: "${ process.env.GIT_VERSION || "unknown GIT_VERSION" }";
 }
-
+*/
 `
+
+function clean(cb) {
+  return require('del')([outDir], cb);
+}
+
 function build() {
-  return gulp.src(['src/**'])
-    .pipe($.if('*.js', $.rev()))
+  return src(['src/**'])
+  .pipe($.if('*.js', $.rev()))
     .pipe($.if('style.css', $.footer(versioncss)))
     .pipe($.if('*.css', $.rev()))
     .pipe($.revReplace())
-    .pipe(gulp.dest(outDir));
+    .pipe(dest(outDir));
 }
 exports.build = series(clean, build);
 
 function dev(cb) {
-  var watcher = watch('src/**', gulp.series(clean, build));
+  var watcher = watch('src/**', series(clean, build));
   watcher.on('all', function(action, path) {
-      console.log('Watcher ' + path + ' (' + action + ')');
+      console.log('Trigger rebuild for ' + path + ' (' + action + ')');
     });
-  // seems to work but needs double Ctrl-C to terminate
+  console.log("Starting reloading development webserver on port 5000, press CTRL-C to quit");
   fb.serve({port:5000}).then(function(){
     watcher.close();
     cb();

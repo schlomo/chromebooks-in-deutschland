2914ec00e28dc3923de3cd0834393c55ec0d653b
Schlomo Schapiro <schlomo+git@schapiro.org>
2019-09-03 16:12:47 +0200

    fix schedule

--- functions/index.js
+++ functions/index.js
@@ -80,7 +80,7 @@ function writeChromebookExpirationData(data) {
     ]);
 }
 
-exports.updateChromebookExpirationData = functions.pubsub.schedule('every 1 day').onRun((context) => {
+exports.updateChromebookExpirationData = functions.pubsub.schedule('every 23 hours').onRun((context) => {
         return getChromebookExpirationData(writeChromebookExpirationData);
     });
 

219724862ee036f7efc5cc492eefc9ba0b73a070
Schlomo Schapiro <schlomo@schapiro.org>
2021-02-10 15:49:52 +0100

    wip

---
+++ aws/certificate/main.tf
@@ -0,0 +1,21 @@
+variable "www_domain_name" {}
+
+variable "root_domain_name" {}
+
+resource "aws_acm_certificate" "website" {
+  provider = "aws_us"
+  domain_name       = "*.${var.root_domain_name}"
+  validation_method = "EMAIL"
+  subject_alternative_names = [
+    var.root_domain_name,
+    var.www_domain_name
+  ]
+}
+
+output "certificate_arn" {
+    value = aws_acm_certificate.website.arn
+}
+
+output "domains" {
+    value = aws_acm_certificate.website.subject_alternative_names
+}
---
+++ aws/website/terragrunt.hcl
@@ -0,0 +1,14 @@
+include {
+  path = find_in_parent_folders()
+}
+
+dependency "certificate" {
+  config_path = "../certificate"
+}
+
+inputs = {
+  certificate_arn = dependency.certificate.outputs.certificate_arn
+  domains = dependency.certificate.outputs.domains
+  www_domain_name = local.www_domain_name
+  root_domain_name = local.root_domain_name
+}

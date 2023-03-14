4ea2a165befd9a965466ca2d88e8cb9b81387f39
Schlomo Schapiro <schlomo+git@schapiro.org>
2023-03-14 18:33:05 +0100

    Add EAN-13 to regex list for matching devices that Idealo only lists under their EAN instead of the variant

--- check-chromebooks.sh
+++ check-chromebooks.sh
@@ -9,38 +9,43 @@
 
 shopt -s globstar
 
-die() { echo 1>&2 "$*" ; exit 1 ; }
+die() {
+    echo 1>&2 "$*"
+    exit 1
+}
 default_regex_parts=(
     # Acer
     '[DN][TQX]\.[A-Z0-9]+\.[0-9][0-9][A-Z0-9]'
     'C[0-9BP]+-[12349NTHLW]+-[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]'
     'C[0-9BPTHLW]+-[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]'
     'C[0-9]+[BPTHLW]+'
-    'CBG[0-9][0-9][0-9]-1H-[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # CBG516-1H
+    'CBG[0-9][0-9][0-9]-1H-[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]'  # CBG516-1H
     'R[0-9][0-9][0-9][A-Z]+-[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # R853TNA-C0EX
-    'R[0-9][0-9][0-9][A-Z]+-[A-Z0-9][A-Z0-9][A-Z0-9]' # R852TN-C1Y which is is an Idealo error for R852TN-C1YV :-(
+    'R[0-9][0-9][0-9][A-Z]+-[A-Z0-9][A-Z0-9][A-Z0-9]'         # R852TN-C1Y which is is an Idealo error for R852TN-C1YV :-(
     # Asus
-    '90[MN][SX][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]-M[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # 90MS01B1-M00080
+    '90[MN][SX][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]-M[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]'        # 90MS01B1-M00080
     'C[MX][0-9][0-9][0-9][0-9][A-Z][A-Z][0-9][A-Z]-[A-Z][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # CM3200FM1A-HW0017
-    'C[MRX][0-9][0-9][0-9][0-9][A-Z][A-Z][A-Z]-[A-Z][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # CR1100FKA-BP0023
-    'CT[0-9][0-9][0-9][A-Z][A-Z]-[A-Z][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # CT100PA-AW0035
-    'C[0-9][0-9][0-9][A-Z][A-Z]-[A-Z][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # C425TA-H50125
+    'C[MRX][0-9][0-9][0-9][0-9][A-Z][A-Z][A-Z]-[A-Z][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]'     # CR1100FKA-BP0023
+    'CT[0-9][0-9][0-9][A-Z][A-Z]-[A-Z][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]'                   # CT100PA-AW0035
+    'C[0-9][0-9][0-9][A-Z][A-Z]-[A-Z][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]'                    # C425TA-H50125
     # Lenovo
     '[28Z][012A][A-Z0-9][A-Z0-9][0-9][0-9][0-9][A-Z0-9]'
     # HP
     '[1-9][2345][abc]-[abcdn][abcdn][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]ng' # 14b-cb0030ng
-    '[1-9][12345]-[abcdn][abcden][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]ng' # 14-cb0030ng
-    '[1-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]EA' # 305R0EA
+    '[1-9][12345]-[abcdn][abcden][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]ng'    # 14-cb0030ng
+    '[1-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]EA'                           # 305R0EA
     # Samsung
     '[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]-[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]' # XE345XDA-KA1DE
+    # Generic
+    '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' # EAN-13
 )
-default_regex="$( tr " " "|" <<<"${default_regex_parts[*]}")"
+default_regex="$(tr " " "|" <<<"${default_regex_parts[*]}")"
 
 test "$1" = "--help" -o "$1" = "-h" && die "Please give regex as first arg, default: $default_regex"
 
 validate=""
 
-if test "$1" == "--validate" ; then
+if test "$1" == "--validate"; then
     validate=1
     shift
 fi
@@ -48,26 +53,26 @@ fi
 regex="${1:-$default_regex}"
 
 function input2devices {
-    tr -C -d "[:print:]" | \
-        egrep -oi "$regex" | \
+    tr -C -d "[:print:]" |
+        egrep -oi "$regex" |
         sort -u
 }
 
-if test "$validate" ; then
+if test "$validate"; then
     chromebooks_file=src/generated/chromebooks.json
     test -s $chromebooks_file || die "Please run 'yarn prep' first to generate Chromebooks data file"
 
     errors=0
     count=0
-    while read -r d ; do
+    while read -r d; do
         let count++
         filtered="$(input2devices <<<"$d")"
-        if test "$d" != "$filtered" ; then
+        if test "$d" != "$filtered"; then
             echo "$d <-> $filtered"
             let errors++
         fi
-    done < <( sed -ne '/variant.*:/s/.*"\(.*\)"/\1/p' $chromebooks_file )
-    if (( errors==0 )) ; then
+    done < <(sed -ne '/variant.*:/s/.*"\(.*\)"/\1/p' $chromebooks_file)
+    if ((errors == 0)); then
         echo "OK All $count devices matched by regex"
         exit 0
     else
@@ -78,13 +83,13 @@ fi
 
 echo "Paste lines and finish with CTRL-D"
 
-devices=( 
-    $( input2devices )
+devices=(
+    $(input2devices)
 )
 res=()
 let red=0 green=0
 for device in "${devices[@]}"; do
-    if grep -q "$device" chromebooks/**/*.yaml ; then
+    if grep -q "$device" chromebooks/**/*.yaml; then
         res+=(" 🟢 $device")
         let green++
     else
